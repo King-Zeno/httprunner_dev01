353a9d917b6f020a3c60fbd12a161293cddf6f5d
ksdd <953532127@qq.com>
2021-05-28 16:30:54 +0800

    增加工程端工作台，客户端

--- login_PC/api_test.py
+++ login_PC/api_test.py
@@ -3,5 +3,5 @@ from httprunner.api import HttpRunner
 
 obj = HttpRunner(log_level="DEBUG")
 
-obj.run('D:/Work/APITest/engineering/httprunner_dev01/api/login.yml')
+obj.run('D:/Work/APITest/engineering/httprunner_dev01/login_PC/login.yml')
 res = obj.summary
--- login_PC/myprofile.py
+++ login_PC/myprofile.py
@@ -3,5 +3,5 @@ from httprunner.api import HttpRunner
 
 obj = HttpRunner(log_level="DEBUG")
 
-obj.run(r'D:\Work\APITest\engineering\httprunner_dev01\api\myprofile.yml')
+obj.run(r'D:\Work\APITest\engineering\httprunner_dev01\login_PC\myprofile.yml')
 res = obj.summary
