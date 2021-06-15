7a2f857ea0d4a82e55dfb1b2440fd862512f7cef
ksdd <953532127@qq.com>
2021-06-15 18:01:20 +0800

    增加更换工程部经理测试用例-测试通过

--- debugtalk.py
+++ debugtalk.py
@@ -2,8 +2,8 @@
 import time
 import os
 
-os.environ['http_proxy'] = 'http://127.0.0.1:8888'
-os.environ['https_proxy'] = 'http://127.0.0.1:8888'
+#os.environ['http_proxy'] = 'http://127.0.0.1:8888'
+#os.environ['https_proxy'] = 'http://127.0.0.1:8888'
 
 def sleep(n_secs):
     time.sleep(n_secs)
